1fa91b412e14778a329d69f28e60d0a66d31b700
Mohamed Wael Tabana <113201941+tabana1@users.noreply.github.com>
2024-06-06 11:34:38 +0300

    up

--- vars/dockerBuildAndPush.groovy
+++ vars/dockerBuildAndPush.groovy
@@ -1,5 +1,12 @@
-def call() {
-    def dockerImage = docker.build("tabana1/your-app:${env.BUILD_NUMBER}")
-    dockerImage.push()
-    return dockerImage.id
+def call(String dockerHubCredentialsID, String imageName) {
+
+	// Log in to DockerHub 
+	withCredentials([usernamePassword(credentialsId: "${dockerHubCredentialsID}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+		sh 'docker login -u ${USERNAME} -p ${PASSWORD}'
+        }
+        
+        // Build and push Docker image
+        echo "Building and Pushing Docker image..."
+        sh "docker build -t ${imageName}:${BUILD_NUMBER} ."
+        sh "docker push ${imageName}:${BUILD_NUMBER}"	 
 }
