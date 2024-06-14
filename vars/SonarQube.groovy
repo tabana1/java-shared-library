f997556e87f2a126150d9931a00dbf3c154e017d
Mohamed Wael Tabana <113201941+tabana1@users.noreply.github.com>
2024-06-14 05:25:29 +0300

    upload

---
+++ vars/SonarQube.groovy
@@ -0,0 +1,17 @@
+def call(Token_Sonar, SONAR_PROJECT_KEY, SONAR_HOST_URL) {
+    script {
+        withCredentials([string(credentialsId: 'Token_Sonar', variable: 'SONAR_TOKEN')]) {
+            // This assumes that the SonarQube Scanner tool is configured in the Jenkins Global Tool Configuration.
+            def scannerHome = tool 'SonarQube'   // Retrieve the installation path of the SonarQube Scanner tool defined in Jenkins.
+            sh """
+                ${scannerHome}/bin/sonar-scanner \
+                -X \
+                -Dsonar.projectKey=${SONAR_PROJECT_KEY} \
+                -Dsonar.host.url=${SONAR_HOST_URL} \
+                -Dsonar.login=${SONAR_TOKEN} \
+                -Dsonar.scm.provider=git \
+                -Dsonar.java.binaries=.
+            """
+        }
+    }
+}
