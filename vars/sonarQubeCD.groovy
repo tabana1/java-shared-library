5ca3a30e595519382b69ee81667ba6f8b7cdc205
Mohamed Wael Tabana <113201941+tabana1@users.noreply.github.com>
2024-06-14 18:15:34 +0300

    up

--- vars/sonarQubeCD.groovy
+++ vars/sonarQubeCD.groovy
@@ -1,8 +1,8 @@
 #!usr/bin/env groovy
 def call(){ 
 	echo "Running SonarQube "
-	withSonarQubeEnv(credentialsId: 'Token_Sonar') {
-		echo "Running SonarQube Analysis..."
+withSonarQubeEnv(credentialsId: 'Token_Sonar') {
+			echo "Running SonarQube Analysis..."
 		sh ''' $SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=ivolve-cd \
 		-Dsonar.java.binaries=. \
 		-Dsonar.projectKey=ivolve-cd '''
