f0647345a9cc4104ab513f86fb944ef88eb28906
Mohamed Wael Tabana <113201941+tabana1@users.noreply.github.com>
2024-06-15 03:45:01 +0300

    up

--- vars/openshiftDeploy.groovy
+++ vars/openshiftDeploy.groovy
@@ -1,10 +1,10 @@
 def call(String OpenshiftcredintialsID, String imageName) {
     
     // Update deployment configuration with new Docker Hub image tag
-    sh "sed -i 's|image:.*|image: ${imageName}:${BUILD_NUMBER}|g' java-deployment-service.yaml"
+    sh "sed -i 's|image:.*|image: ${imageName}:${BUILD_NUMBER}|g' deployment.yaml"
     
 
     withKubeCredentials(kubectlCredentials: [[caCertificate: '', clusterName: '', contextName: '', credentialsId: 'open-shift-service', namespace: 'mohamedtabana', serverUrl: 'https://console-openshift-console.apps.ocp-training.ivolve-test.com']]) {
-        sh "oc apply -f java-deployment-service.yaml -n mohamedtabana"
+        sh "oc apply -f deployment.yaml -n mohamedtabana"
     }
 }
